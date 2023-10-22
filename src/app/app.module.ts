1efee2b48626cd88fee87697a147735a442468c1
Natalia Graciano <nataliavgraciano@gmail.com>
2023-10-22 18:47:40 -0300

    Making the content in page dinamic
    Signed-off-by: Natalia Graciano <nataliavgraciano@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,9 @@ import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
 import { MenuTitleComponent } from './components/menu-title/menu-title.component';
 import { BigCardComponent } from './components/big-card/big-card.component';
 import { SmallCardComponent } from './components/small-card/small-card.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ContentComponent } from './pages/content/content.component';
+
 
 @NgModule({
   declarations: [
@@ -14,7 +17,9 @@ import { SmallCardComponent } from './components/small-card/small-card.component
     MenuBarComponent,
     MenuTitleComponent,
     BigCardComponent,
-    SmallCardComponent
+    SmallCardComponent,
+    HomeComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
